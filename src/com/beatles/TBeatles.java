d5f8cd345ec6024948f68736ed75a2b64cb08b76
Vasuki Vardhan <vasukivardhan96@gmail.com>
2018-12-05 09:43:00 +0530

    Corrected for Unque Constraint Violation

--- src/com/beatles/TBeatles.java
+++ src/com/beatles/TBeatles.java
@@ -9,53 +9,61 @@ import java.sql.Date;
 @Table(name= "beatles")
 
 public class TBeatles {
-    @Id
     private String albumName;
     private int relYear,ep,lp,sp;
     private java.sql.Date relDate;
 
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+
+    @Column(name="albumName")
     public String getAlbum(){
         return albumName;
-    }
-
-    public int getRelYear() {
-        return relYear;
-    }
-
-    public int getEp() {
-        return ep;
-    }
-
-    public int getLp(){
-        return lp;
-    }
-
-    public int getSp() {
-        return sp;
-    }
-
-    public java.sql.Date getRelDate() {
-        return relDate;
     }
 
     public void setAlbumName(String albumName){
         this.albumName=albumName;
     }
 
+    @Column(name="relYear")
+    public int getRelYear() {
+        return relYear;
+    }
+
     public void setRelYear(int relYear){
         this.relYear=relYear;
+    }
+
+    @Column(name="ep")
+    public int getEp() {
+        return ep;
     }
 
     public void setEp(int ep) {
         this.ep=ep;
     }
 
+    @Column(name = "lp")
+    public int getLp(){
+        return lp;
+    }
+
     public void setLp(int lp) {
         this.lp = lp;
     }
 
+    @Column(name = "sp")
+    public int getSp() {
+        return sp;
+    }
+
     public void setSp(int sp) {
         this.sp=sp;
+    }
+
+    @Column(name = "relDate")
+    public java.sql.Date getRelDate() {
+        return relDate;
     }
 
     public void setRelDate(java.sql.Date relDate) {
